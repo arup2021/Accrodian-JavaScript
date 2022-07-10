b179bc7d1ad4f07ab01d4d9feb8e72cdaa0f58a5
Arup Ratan Sarker <59112549+arup2021@users.noreply.github.com>
2022-07-10 23:27:55 +0600

    Add files via upload

---
+++ accrodian/accrodian.js
@@ -0,0 +1,19 @@
+var example = document.getElementsByClassName("accrodian_item");
+for (i = 0; i < example.length; ++i) {
+    example[i].onclick = function(){
+        var current = this;
+        for (var i = 0; i < example.length; i++) {
+            if (current != example[i]) {
+                example[i].classList.remove('active');
+                example[i].getElementsByClassName("accrodian_description")[0].style.maxHeight = "0px";
+            } else if (current.classList.contains('active') === true) {
+                current.classList.remove('active');
+                current.getElementsByClassName("accrodian_description")[0].style.maxHeight = "0px";
+            }else {
+                current.classList.add('active')
+                var des_height = current.getElementsByClassName("accrodian_description")[0];
+                des_height.style.maxHeight = des_height.scrollHeight + "px";
+            }
+        }
+    }
+}
